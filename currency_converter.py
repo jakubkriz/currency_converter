4d00b6e89923e6ae6d47e9370cae6ad0eec939e1
jakubkriz <jakubkriz92@gmail.com>
2017-07-03 18:36:46 +0200

    working with command line arguments

--- currency_converter.py
+++ currency_converter.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--amount', type=float)
+    parser.add_argument('--input_currency')
+    parser.add_argument('--output_currency')
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    print get_arguments()
