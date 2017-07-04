60ff332456ed5e07ac170675ca22a5f4d188cd0c
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 15:59:51 +0200

    cleaning up testing code

--- tests/test_parser_helper.py
+++ tests/test_parser_helper.py
@@ -10,59 +10,48 @@ class TestParserHelper(unittest.TestCase):
     def test_is_currency_code(self): 
         # A valid currency code must be recognized
         currency_code = 'CZK'
-        is_curr_code = ParserHelper.is_currency_code(currency_code)
-        self.assertTrue(is_curr_code)
+        self.assertTrue(ParserHelper.is_currency_code(currency_code))
 
         # A valid currency code can also be surrounded by whitespace characters
         currency_code = '  EUR    '
-        is_curr_code = ParserHelper.is_currency_code(currency_code)
-        self.assertTrue(is_curr_code)
+        self.assertTrue(ParserHelper.is_currency_code(currency_code))
 
         # Number is not a currency code
         currency_code = '123'
-        is_curr_code = ParserHelper.is_currency_code(currency_code)
-        self.assertFalse(is_curr_code)
+        self.assertFalse(ParserHelper.is_currency_code(currency_code))
 
         # Currency code must consist of 3 capital letters
         currency_code = 'CZKs'
-        is_curr_code = ParserHelper.is_currency_code(currency_code)
-        self.assertFalse(is_curr_code)
+        self.assertFalse(ParserHelper.is_currency_code(currency_code))
 
         # Currency code must a valid currency
         currency_code = 'AAA'
-        is_curr_code = ParserHelper.is_currency_code(currency_code)
-        self.assertFalse(is_curr_code)
+        self.assertFalse(ParserHelper.is_currency_code(currency_code))
 
     def test_is_currency_symbol(self):
         # A valid currency symbol must be recognized
         currency_symbol = 'Kč' 
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertTrue(is_curr_symbol)
+        self.assertTrue(ParserHelper.is_currency_symbol(currency_symbol))
 
         # A valid currency symbol can also be surrounded by whitespace characters
         currency_symbol = '  € '
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertTrue(is_curr_symbol)
+        self.assertTrue(ParserHelper.is_currency_symbol(currency_symbol))
 
         # Vietnamese đồng must be recognized as a valid currency symbol
         currency_symbol = '₫'
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertTrue(is_curr_symbol)
+        self.assertTrue(ParserHelper.is_currency_symbol(currency_symbol))
 
         # Numbers are not a valid currency symbol
         currency_symbol = '12'
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertFalse(is_curr_symbol)
+        self.assertFalse(ParserHelper.is_currency_symbol(currency_symbol))
 
         # Random characters are not a valid currency symbol
         currency_symbol = 'abSbsas'
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertFalse(is_curr_symbol)
+        self.assertFalse(ParserHelper.is_currency_symbol(currency_symbol))
 
         # Number sign is not a valid currency symbol
         currency_symbol = '#'
-        is_curr_symbol = ParserHelper.is_currency_symbol(currency_symbol)
-        self.assertFalse(is_curr_symbol)
+        self.assertFalse(ParserHelper.is_currency_symbol(currency_symbol))
 
     # def tearDown(self):
 
