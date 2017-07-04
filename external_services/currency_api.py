384291ef3d2a5d033babb4d8a2b7c5ba9ea61cf8
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 18:10:36 +0200

    getting available currencies from API

--- external_services/currency_api.py
+++ external_services/currency_api.py
@@ -2,14 +2,32 @@
 
 import requests
 
+import strings
+
 class CurrencyAPI:
     """
     Represents a service used to get a currency rate between given currencies
     """
-
     @staticmethod
     def get_rate(currency_input, currency_output):
         url = u"http://api.fixer.io/latest?base={0}&symbols={1}"
         complete_url = url.format(currency_input, currency_output)
         response = requests.get(complete_url).json()
-        return response[u'rates'][currency_output]
+        try:
+            return response[u'rates'][currency_output]
+        except KeyError as exc:
+            print strings.API_RATE_ERROR
+            return None
+
+    @staticmethod
+    def get_available_currencies():
+        url = u"http://api.fixer.io/latest"
+        response = requests.get(url).json()
+        currencies = []
+        try:        
+            for currency in response[u'rates'].keys():
+                currencies.append(currency)           
+        except KeyError as exc:
+            print strings.API_CURRENCIES_ERROR
+        return currencies
+
--- strings.py
+++ strings.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 INVALID_INPUT = "{0} is not a valid currency code, nor a currency symbol."
+API_RATE_ERROR = "Cannot obtain currency rate from the Currency API."
+API_CURRENCIES_ERROR = "Cannot obtain currencies from the Currency API."
