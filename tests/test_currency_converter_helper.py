ed0914f34d0d92fca32fc33b8b2615373a11fa76
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 13:04:01 +0200

    setting up 1st test

---
+++ tests/test_currency_converter_helper.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from helpers.currency_converter_helper import CurrencyConverterHelper
+
+class TestCurrencyConverterHelper(unittest.TestCase):
+    # def setUp(self):
+
+    # def test_is_currency_code(self): 
+
+    # def test_is_currency_code(self):     
+
+    # def tearDown(self):
+
