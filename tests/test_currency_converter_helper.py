5461f73142dceee4f03c387c1dbfb11e39eded5f
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 15:21:23 +0200

    adding test for is_currency_code method

--- tests/test_currency_converter_helper.py
+++ tests/test_currency_converter_helper.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import unittest
+
 from helpers.currency_converter_helper import CurrencyConverterHelper
 
 class TestCurrencyConverterHelper(unittest.TestCase):
     # def setUp(self):
 
-    # def test_is_currency_code(self): 
+    def test_is_currency_code(self): 
+        currency_code = 'CZK'
+        is_curr_code = CurrencyConverterHelper.is_currency_code(currency_code)
+        self.assertEqual(is_curr_code, True)
 
-    # def test_is_currency_code(self):     
+        # Number is not a currency code
+        currency_code = '123'
+        is_curr_code = CurrencyConverterHelper.is_currency_code(currency_code)
+        self.assertEqual(is_curr_code, False)
+
+        # Currency code must consist of 3 capital letters
+        currency_code = 'CZKs'
+        is_curr_code = CurrencyConverterHelper.is_currency_code(currency_code)
+        self.assertEqual(is_curr_code, False)
+
+        # Currency code must a valid currency
+        currency_code = 'AAA'
+        is_curr_code = CurrencyConverterHelper.is_currency_code(currency_code)
+        self.assertEqual(is_curr_code, False)
+
+    # def test_is_currency_symbol(self):     
 
     # def tearDown(self):
 
