f56fcefc64ea4a469019ddc025d6a5c59c4acd49
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 20:04:23 +0200

    managing output currencies

--- conversion_manager.py
+++ conversion_manager.py
@@ -23,17 +23,8 @@ class ConversionManager:
             input_currency = self.__get_currency_from_symbol(input_currency)
             if not input_currency:
                 print strings.SYMBOL_ERROR.format(input_currency)
-                
+                return None
         return input_currency
-
-    def __get_currency_from_symbol(self, input_currency):
-        with open('currency_symbols.json') as file:
-            currencies = json.load(file)
-            for symbol, codes in currencies.iteritems():
-                if input_currency.decode('utf-8') == symbol:
-                    # Return the 1st currency code for the given symbol
-                    return codes[0]
-        return None
 
     def __get_output_currencies(self, output_currency):
         if not output_currency:
@@ -44,11 +35,22 @@ class ConversionManager:
                 self.__store_currencies_in_file(output_currencies)
             return output_currencies
         else:
-            if ParserHelper.is_currency_symbol(input_currency):
-                # TODO
-                return 
-            else:
-                return [output_currency]
+            if ParserHelper.is_currency_symbol(output_currency):
+                output_currency = \
+                    self.__get_currency_from_symbol(output_currency)
+                if not output_currency:
+                    print strings.SYMBOL_ERROR.format(output_currency)
+                    return None
+            return [output_currency]
+
+    def __get_currency_from_symbol(self, input_currency):
+        with open('currency_symbols.json') as file:
+            currencies = json.load(file)
+            for symbol, codes in currencies.iteritems():
+                if input_currency.decode('utf-8') == symbol:
+                    # Return the 1st currency code for the given symbol
+                    return codes[0]
+        return None
 
     def __store_currencies_in_file(self, currencies):
         with open('currencies.txt', 'w') as file:
