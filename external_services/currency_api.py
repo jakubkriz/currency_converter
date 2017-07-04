9d03100f45000535dacd947450f70edc4c249e91
jakubkriz <jakubkriz92@gmail.com>
2017-07-04 18:20:54 +0200

    handling errors in the api calls

--- external_services/currency_api.py
+++ external_services/currency_api.py
@@ -4,7 +4,7 @@ import requests
 
 import strings
 
-class CurrencyAPI:
+class CurrencyApi:
     """
     Represents a service used to get a currency rate between given currencies
     """
@@ -12,22 +12,33 @@ class CurrencyAPI:
     def get_rate(currency_input, currency_output):
         url = u"http://api.fixer.io/latest?base={0}&symbols={1}"
         complete_url = url.format(currency_input, currency_output)
-        response = requests.get(complete_url).json()
         try:
+            response = requests.get(complete_url, timeout=3).json()
+            if not response.status_code == 200:
+                print strings.API_ERROR.format(response.status_code)
+                return None
             return response[u'rates'][currency_output]
+        except requests.Timeout as exc:
+            print strings.API_TIMEOUT
         except KeyError as exc:
             print strings.API_RATE_ERROR
-            return None
+        return None
 
     @staticmethod
     def get_available_currencies():
         url = u"http://api.fixer.io/latest"
-        response = requests.get(url).json()
         currencies = []
         try:        
+            response = requests.get(url, timeout=3).json()
+            if not response.status_code == 200:
+                print strings.API_ERROR.format(response.status_code)
+                return None
             for currency in response[u'rates'].keys():
-                currencies.append(currency)           
+                currencies.append(currency) 
+            return currencies          
+        except requests.Timeout as exc:
+            print strings.API_TIMEOUT
         except KeyError as exc:
             print strings.API_CURRENCIES_ERROR
-        return currencies
+        return None
 
--- strings.py
+++ strings.py
@@ -2,3 +2,5 @@
 INVALID_INPUT = "{0} is not a valid currency code, nor a currency symbol."
 API_RATE_ERROR = "Cannot obtain currency rate from the Currency API."
 API_CURRENCIES_ERROR = "Cannot obtain currencies from the Currency API."
+API_TIMEOUT = "Cannot reach Currency API. Request timed out."
+API_ERROR = "Request unsuccessful, ended with status code {0}."
